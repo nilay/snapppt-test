6b25fa0d4e1ad9e5b3da39235a8f3398da3c323c
Nilay <nilayanand@gmail.com>
2018-03-23 01:12:39 +0530

    Fix indentation

--- json_stream_parser.rb
+++ json_stream_parser.rb
@@ -1,63 +1,59 @@
 require "JSON"
 class JsonStreamParser
 
-	BUFFER_SIZE   = 1024
+  BUFFER_SIZE   = 1024
 
-	def initialize path
+  def initialize path
     raise ArgumentError unless File.exists?( path )
 
     @json_file           = File.open( path )
-
-    
-    @file_seek_offset 	= 0 
-    @stack_object 			= 0
-
-
+    @file_seek_offset   = 0 
+    @stack_object       = 0
   end
   
   def get_block
-  	return nil unless @json_file.tell < @json_file.stat.size
+    return nil unless @json_file.tell < @json_file.stat.size
 
-  	@json_file.seek(@file_seek_offset)
-  	@file_seek_offset += BUFFER_SIZE
-  	@json_file.read( BUFFER_SIZE )
+    @json_file.seek(@file_seek_offset)
+    @file_seek_offset += BUFFER_SIZE
+    @json_file.read( BUFFER_SIZE )
   end
 
 
   def adjust_stack(letter)
-    @stack_object += 1 	if letter == '{'
-    @stack_object -= 1 	if letter == '}'	
-  end  	
+    @stack_object += 1  if letter == '{'
+    @stack_object -= 1  if letter == '}'  
+  end   
 
-  	
+    
   def stream
 
     object_start_position = -1
     previous_data = ''
 
-  	while data = get_block
-  		index = 0
-  		data = previous_data + data
+    while data = get_block
+      index = 0
+      data = previous_data + data
       data.split('').each { |c| 
-      	adjust_stack c
-      	
-      	object_start_position = index if @stack_object == 1 && c == '{'
-      	
-      	
-    		# yeild json if  object is completed
-    		yield JSON.parse(data[object_start_position, (index - object_start_position) +1 ]).tap { |hs| hs.delete("_id") } if @stack_object == 0 && c == '}' && object_start_position > -1 
+        adjust_stack c
+        
+        object_start_position = index if @stack_object == 1 && c == '{'
+        
+        
+        # yeild json if  object is completed
+        yield JSON.parse(data[object_start_position, (index - object_start_position) +1 ]).tap { |hs| hs.delete("_id") } if @stack_object == 0 && c == '}' && object_start_position > -1 
     
-      	index += 1
-			}
+        index += 1
+      }
 
-			if @stack_object == 1
-				# current block do not end the opening json object. Keep the remaining string to be added in next block
-				previous_data = data[object_start_position..-1]
-				@stack_object 			= 0
-			end	
+      if @stack_object == 1
+        # current block do not end the opening json object. Keep the remaining string to be added in next block
+        previous_data = data[object_start_position..-1]
+        @stack_object       = 0
+      end 
 
     end  
 
-  end	
+  end 
 
 end  
--- parse.rb
+++ parse.rb
@@ -5,14 +5,14 @@ file_counter = index = 1
 collections = []
 
 jsp.stream do |data|
-	collections << data
-	if index % 10 == 0
-		File.open("out#{file_counter}.json", 'w') {|f| f.write(collections.to_json) }
-		collections = []
-		file_counter += 1
-	end	
+  collections << data
+  if index % 10 == 0
+    File.open("out#{file_counter}.json", 'w') {|f| f.write(collections.to_json) }
+    collections = []
+    file_counter += 1
+  end 
 
-	index += 1
-end	
+  index += 1
+end 
 
 File.open("out#{file_counter}.json", 'w') {|f| f.write(collections.to_json) } if collections.count > 0
