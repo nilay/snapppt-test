f78ab17e4de027548853a6d2c97d7663456e8063
Nilay <nilayanand@gmail.com>
2018-03-23 01:06:56 +0530

    Initial code commit

---
+++ parse.rb
@@ -0,0 +1,18 @@
+require_relative 'json_stream_parser'
+jsp = JsonStreamParser.new '1000-users.json'
+
+file_counter = index = 1
+collections = []
+
+jsp.stream do |data|
+	collections << data
+	if index % 10 == 0
+		File.open("out#{file_counter}.json", 'w') {|f| f.write(collections.to_json) }
+		collections = []
+		file_counter += 1
+	end	
+
+	index += 1
+end	
+
+File.open("out#{file_counter}.json", 'w') {|f| f.write(collections.to_json) } if collections.count > 0
