4aea26463a6abaf51deb9f46b5f12aa521655223
Nilay <nilayanand@gmail.com>
2018-03-23 16:52:30 +0530

    Remove empty array from json

--- json_stream_parser.rb
+++ json_stream_parser.rb
@@ -23,7 +23,22 @@ class JsonStreamParser
   def adjust_stack(letter)
     @stack_object += 1  if letter == '{'
     @stack_object -= 1  if letter == '}'  
-  end   
+  end 
+
+  def to_json(json_string)
+    # make hash from json string. Remove _id attributes as well
+    return if json_string.nil?
+    json_obj = JSON.parse(json_string).tap { |hs| hs.delete("_id") } 
+
+    # Remove empty array
+    json_obj = json_obj.tap { |hs| hs.delete("mentions") }  if json_obj.key?('mentions') && json_obj['mentions'].empty?
+    json_obj = json_obj.tap { |hs| hs.delete("hashtags") }  if json_obj.key?('hashtags') && json_obj['hashtags'].empty?
+
+    # remove non alpha numeric character from bio
+    json_obj['bio'] = json_obj['bio'].gsub(/[^0-9a-z ]/i, '') if json_obj.key?('bio')
+
+    json_obj
+  end  
 
     
   def stream
@@ -32,14 +47,17 @@ class JsonStreamParser
     previous_data = ''
 
     while data = get_block
+
       index = 0
       data = previous_data + data
+
       data.split('').each { |c| 
         adjust_stack c
         object_start_position = index if @stack_object == 1 && c == '{'
         
         # yeild json if  object is completed
-        yield JSON.parse(data[object_start_position, (index - object_start_position) +1 ]).tap { |hs| hs.delete("_id") } if @stack_object == 0 && c == '}' && object_start_position > -1 
+        yield to_json(data[object_start_position, (index - object_start_position) +1 ]) if @stack_object == 0 && c == '}' && object_start_position > -1 
+       
         index += 1
       }
 
@@ -47,10 +65,11 @@ class JsonStreamParser
         # current block has not been completed the opening json object. Keep the remaining string to be added in next block
         previous_data = data[object_start_position..-1]
         @stack_object       = 0
+
+      else
+        previous_data = ''
       end 
-
     end  
-
   end 
-
+  
 end  
--- parse.rb
+++ parse.rb
@@ -1,18 +1,16 @@
 require_relative 'json_stream_parser'
 jsp = JsonStreamParser.new '1000-users.json'
 
-file_counter = index = 1
+file_counter = index = 0
 collections = []
 
 jsp.stream do |data|
   collections << data
-  if index % 10 == 0
+  if (index+=1) % 100 == 0
     File.open("out#{file_counter}.json", 'w') {|f| f.write(collections.to_json) }
     collections = []
     file_counter += 1
   end 
-
-  index += 1
 end 
 
 File.open("out#{file_counter}.json", 'w') {|f| f.write(collections.to_json) } if collections.count > 0
